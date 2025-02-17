903e15248928de51657fdeab5b2fbc27f2179010
Himanshu Parchand <himan2104@outlook.com>
2025-02-17 10:24:41 +0530

    Note for floating types

--- Include/Core/Definitions.hpp
+++ Include/Core/Definitions.hpp
@@ -15,7 +15,14 @@ using UInt16 = std::uint16_t;
 using UInt32 = std::uint32_t;
 using UInt64 = std::uint64_t;
 
-// using Float16 = ;
+// NOTE: The following floating types are not avaialble
+// even when using C++23 on my compiler [GCC 14.2.0]
+// using Float16  = std::float16_t;
+// using Float32  = std::float32_t;
+// using Float64  = std::float64_t;
+// using Float128 = std::float128_t;
+
 using Float32 = float;
 using Float64 = double;
+
 } // namespace Core
